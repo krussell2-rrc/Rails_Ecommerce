c551477cf12254f67cb741d32095387f76d1fe31
krussell2 <krussell2@academic.rrc.ca>
2025-04-04 11:31:15 -0500

    Generated the Pages controller with about and contact actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "pages/about"
+  get "pages/contact"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
