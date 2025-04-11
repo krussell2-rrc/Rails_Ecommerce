8f9039db9249bd059c32cbe34c469e56a20d3a65
krussell2 <krussell2@academic.rrc.ca>
2025-04-10 23:25:33 -0500

    Added functionality to view sort products by their create and update date

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,18 @@
 class ProductsController < ApplicationController
   def index
     @q = Product.ransack(params[:q])
-    @products = @q.result(distinct: true).page(params[:page]).per(15)
+    base_result = @q.result(distinct: true)
+
+    case params[:sort]
+    when "Newest Products"
+      @products = base_result.order(created_at: :desc)
+    when "Recently Updated"
+      @products = base_result.order(updated_at: :desc)
+    else
+      @products = base_result
+    end
+
+    @products = @products.page(params[:page]).per(15)
   end
 
   def show
