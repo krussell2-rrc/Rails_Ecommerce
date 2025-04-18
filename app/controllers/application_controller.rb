dc4d1675ae5f448c2cf48b66d4d6366c65014fa2
krussell2 <krussell2@academic.rrc.ca>
2025-04-18 10:33:04 -0500

    Generated Cart controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  helper_method :current_cart
   def current_cart
     if session[:cart_id]
       Cart.find_by(id: session[:cart_id]) || create_new_cart
