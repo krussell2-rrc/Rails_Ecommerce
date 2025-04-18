7573ee2e01fbedf3b78f716c87ffba9ce024c9ea
krussell2 <krussell2@academic.rrc.ca>
2025-04-18 10:26:29 -0500

    Added functionality to find cart by the session[:cart_id], if none is found a new cart is created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def current_cart
+    if session[:cart_id]
+      Cart.find_by(id: session[:cart_id]) || create_new_cart
+    else
+      create_new_cart
+    end
+  end
+
+  private
+
+  def create_new_cart
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
 end
