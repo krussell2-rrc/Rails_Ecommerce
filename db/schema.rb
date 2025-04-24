2353a7bce0d922fc9179f60b0ffb52b05a917358
krussell2 <krussell2@academic.rrc.ca>
2025-04-24 14:56:08 -0500

    Added province_name field to Provinces model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_24_182013) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_24_195459) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -126,6 +126,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_24_182013) do
     t.decimal "hst_rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "province_name"
   end
 
   create_table "users", force: :cascade do |t|
