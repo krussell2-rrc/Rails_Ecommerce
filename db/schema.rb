a411835762247f3f4414b568145bc4162e7686c3
krussell2 <krussell2@academic.rrc.ca>
2025-04-24 11:58:24 -0500

    Created the Customer table to store customer's personal information

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_24_164544) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_24_165453) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -68,6 +68,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_24_164544) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "customers", force: :cascade do |t|
+    t.string "full_name"
+    t.string "phone_number"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_customers_on_user_id"
+  end
+
   create_table "pages", force: :cascade do |t|
     t.string "name"
     t.text "content"
@@ -106,5 +115,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_24_164544) do
   add_foreign_key "addresses", "provinces"
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "products"
+  add_foreign_key "customers", "users"
   add_foreign_key "products", "categories"
 end
