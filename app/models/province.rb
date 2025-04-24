c990663d3e588a83ab6cd335a7e621e30a12d386
krussell2 <krussell2@academic.rrc.ca>
2025-04-24 15:28:26 -0500

    Seeded the Province model from a JSON containing the provinces and their respective GST, PST, and HST rates

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,5 +1,5 @@
 class Province < ApplicationRecord
-  validates :province_code, :gst_rate, :pst_rate, :hst_rate, presence: true
-  validates :gst_rate, :pst_rate, :hst_rate, numericality: true
+  validates :province_code, :province_name, presence: true
+  validates :gst_rate, :pst_rate, :hst_rate, numericality: true, allow_nil: true
   validates :province_code, length: { maximum: 2 }
 end
