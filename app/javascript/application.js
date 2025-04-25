7741fbfe9d812971053628b58bc02ffd5c1d59ba
krussell2 <krussell2@academic.rrc.ca>
2025-04-25 10:44:38 -0500

    Added JS functionality to display the total after taxes applied to the subtotal

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -20,18 +20,20 @@ document.addEventListener("turbo:load", () => {
           fetch('/canadian_tax_rates.json')
           .then(response => response.json())
           .then(data => {
-            const GST = data.find(p => p.dataId === Number(item.dataset.id));
-            const PST = data.find(p => p.dataId === Number(item.dataset.id));
-            const HST = data.find(p => p.dataId === Number(item.dataset.id));
+            const tax = data.find(p => p.dataId === Number(item.dataset.id));
 
+            const subtotal = Number(document.querySelector('.product-subtotal').innerText)
+            const totalTaxRate = tax["gst"] + tax["pst"] + tax["hst"]
+            const total = subtotal * (1 + totalTaxRate)
+            const roundedTotal = total.toFixed(2)
 
             let taxRateContainer = document.getElementById("tax-rates")
             taxRateContainer.innerHTML =
-            `<p class="ml-5 is-size-4 has-text-weight-bold has-text-white">Taxes:</p>
-            <p class="ml-5 is-size-6 has-text-weight-bold has-text-white" id="gst-rate">GST: <span>${GST["gst"]}%</span></p>
-            <p class="ml-5 is-size-6 has-text-weight-bold has-text-white" id="pst-rate">PST: <span>${PST["pst"]}%</span></p>
-            <p class="ml-5 is-size-6 has-text-weight-bold has-text-white" id="hst-rate">HST: <span>${HST["hst"]}%</span></p>`
-
+            `<p class="ml-5 is-size-6 has-text-weight-bold has-text-white">Taxes:</p>
+            <p class="ml-5 is-size-6 has-text-weight-bold has-text-white" id="gst-rate">GST: <span class="is-size-6 has-text-weight-normal has-text-white">${tax["gst"]}%</span></p>
+            <p class="ml-5 is-size-6 has-text-weight-bold has-text-white" id="pst-rate">PST: <span class="is-size-6 has-text-weight-normal has-text-white">${tax["pst"]}%</span></p>
+            <p class="ml-5 is-size-6 has-text-weight-bold has-text-white" id="hst-rate">HST: <span class="is-size-6 has-text-weight-normal has-text-white">${tax["hst"]}%</span></p>
+            <p class="ml-5 mt-5 is-size-4 has-text-weight-bold has-text-white" id="hst-rate">Total: <span class="is-size-4 has-text-weight-normal has-text-white">$${roundedTotal}</span></p>`
           }).catch(error => {
             console.error('Fetch error:', error);
           });
