6a08a800dd5468f0a4347e2b6ba91dbb16ca38da
krussell2 <krussell2@academic.rrc.ca>
2025-04-27 09:49:26 -0500

    Generated the User model using the devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :products, only: [ :index, :show ]
   resources :addresses, only: [ :create ]
   resources :cart_items, only: [ :update, :destroy ]
