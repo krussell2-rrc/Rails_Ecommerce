f8a9b0e64607b3fc714fb33c2233da0b2c46e155
krussell2 <krussell2@academic.rrc.ca>
2025-04-27 09:40:18 -0500

    Dropped User model to recreate with devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_27_013136) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_27_143803) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -153,14 +153,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_27_013136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "province_name"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_hash"
-    t.string "email_address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "addresses", "provinces"
