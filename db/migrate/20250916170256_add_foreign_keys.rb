03d5bec23cf06321de12c8e9b6e6ecad3dade54a
krussell2 <krussell2@academic.rrc.ca>
2025-09-16 12:53:46 -0500

    Make AddForeignKeys idempotent (guard with foreign_key_exists?)

--- db/migrate/20250916170256_add_foreign_keys.rb
+++ db/migrate/20250916170256_add_foreign_keys.rb
@@ -1,14 +1,42 @@
 class AddForeignKeys < ActiveRecord::Migration[8.0]
-  def change
-    add_foreign_key :addresses,       :provinces
-    add_foreign_key :addresses,       :users
-    add_foreign_key :cart_items,      :carts
-    add_foreign_key :cart_items,      :products
-    add_foreign_key :invoice_products,:invoices
-    add_foreign_key :invoice_products,:products
-    add_foreign_key :order_items,     :orders
-    add_foreign_key :order_items,     :products
-    add_foreign_key :products,        :categories
-    add_foreign_key :users,           :provinces
+  def up
+    add_fk(:addresses,        :provinces)
+    add_fk(:addresses,        :users)
+    add_fk(:cart_items,       :carts)
+    add_fk(:cart_items,       :products)
+    add_fk(:invoice_products, :invoices)
+    add_fk(:invoice_products, :products)
+    add_fk(:order_items,      :orders)
+    add_fk(:order_items,      :products)
+    add_fk(:orders,           :customers)
+    add_fk(:products,         :categories)
+    add_fk(:users,            :provinces)
+  end
+
+  def down
+    # safe rollback: only remove if present
+    rm_fk(:addresses,        :provinces)
+    rm_fk(:addresses,        :users)
+    rm_fk(:cart_items,       :carts)
+    rm_fk(:cart_items,       :products)
+    rm_fk(:invoice_products, :invoices)
+    rm_fk(:invoice_products, :products)
+    rm_fk(:order_items,      :orders)
+    rm_fk(:order_items,      :products)
+    rm_fk(:orders,           :customers)
+    rm_fk(:products,         :categories)
+    rm_fk(:users,            :provinces)
+  end
+
+  private
+
+  def add_fk(from, to, **opts)
+    return if foreign_key_exists?(from, to, **opts)
+    add_foreign_key(from, to, **opts)
+  end
+
+  def rm_fk(from, to, **opts)
+    return unless foreign_key_exists?(from, to, **opts)
+    remove_foreign_key(from, to, **opts)
   end
 end
